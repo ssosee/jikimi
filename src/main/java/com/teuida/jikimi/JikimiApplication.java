2ecf98ecca32e1b0c5122770db31f188f84e7977
howisitgoing <howisitgoing@kakao.com>
2026-01-05 22:55:20 +0900

    Initial commit

---
+++ src/main/java/com/teuida/jikimi/JikimiApplication.java
@@ -0,0 +1,13 @@
+package com.teuida.jikimi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JikimiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JikimiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teuida/jikimi/JikimiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teuida.jikimi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JikimiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
